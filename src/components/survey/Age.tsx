9c83a8d2f050e2e819f9fe3e3c0612cfd6d28d30
Kim-Jaemin420 <jany240@naver.com>
2021-10-06 13:40:18 +0900

    Feat: 나이 설문 컴포넌트 생성

---
+++ src/components/survey/Age.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+interface Props {
+  age: number;
+  setAge: (value: number) => void;
+  setPageCount: (page: number) => void;
+}
+
+function Age({ age, setAge, setPageCount }: Props): JSX.Element {
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const { value }: { value: string } = e.target;
+
+    setAge(+value);
+  };
+
+  const onSubmit = () => {
+    setPageCount(3);
+  };
+
+  return (
+    <div>
+      <span>나이를 알려주세요</span>
+      <input type="text" placeholder="00" value={age} onChange={onChange} />
+      <button type="button" onClick={onSubmit}>
+        다음
+      </button>
+    </div>
+  );
+}
+
+export default Age;
