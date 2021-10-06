3975a49819437784e417ec58c392d4817503ba56
Kim-Jaemin420 <jany240@naver.com>
2021-10-06 13:39:06 +0900

    Modify: setState 함수 이름 변경, props로 Input value 받아옴

--- src/components/survey/Nickname.tsx
+++ src/components/survey/Nickname.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
 interface Props {
-  setState: (value: string) => void;
+  nickname: string;
+  setNickname: (value: string) => void;
   setPageCount: (page: number) => void;
 }
 
-function Nickname({ setState, setPageCount }: Props) {
+function Nickname({ nickname, setNickname, setPageCount }: Props) {
   const onChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const { value }: { value: string } = e.target;
 
-    setState(value);
+    setNickname(value);
   };
 
   const onSubmit = () => {
@@ -20,7 +21,7 @@ function Nickname({ setState, setPageCount }: Props) {
     <div className="usernamewrap">
       <div className="usernamebody">
         <h2>당신을 뭐라고 불러드릴까요?</h2>
-        <input type="text" onChange={onChange} />
+        <input type="text" value={nickname} onChange={onChange} />
         <h3>라고 불러줘</h3>
         <button className="next__button" type="button" onClick={onSubmit}>
           다음
